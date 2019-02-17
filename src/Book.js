cf282addefee9175cfeae4ac376659b956d26c9c
dwcleb <dwcleb@gmail.com>
2019-02-17 13:21:45 -0300

    Add file Book.js, BookLis.js, SearchBook.js. Install React Router and PropTypes. Book.js script created

---
+++ src/Book.js
@@ -0,0 +1,45 @@
+import React from 'react' 
+import PropTypes from 'prop-types' 
+import * as BookAPI from './BooksAPI' // API functions
+
+// Book Class
+const Book = props => {
+  const { book, onChangeShelf } = props
+  const { imageLinks, shelf, title, authors } = book
+  return (
+    <div className="book">
+      <div className="book-top">
+      {/* Book cover imagem */}
+        <div
+          className="book-cover"
+          styles={{
+            width: 128,
+            heigth: 192,
+            backgroundImage: `url(${imageLinks.thumbnail})`
+        }}></div>
+        {/* Book options */}
+        <div className="book-shelf-changer">
+          <select
+            value={shelf}
+            onChange={ (event) => { onChangeShelf(event, book) }}
+          >
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+      </div>
+      {/* Book information: Title and Authors */}
+      <div className="book-title">{title}</div>
+      <div className="book-authors">{authors}</div>
+    </div>
+  )
+}
+
+Book.propTypes = {
+  book: PropTypes.array.isRequired,
+  onChangeShelf: PropTypes.func.isRequired
+}
+export default Book
+
